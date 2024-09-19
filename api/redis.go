2b26aa3252a88ef12a5c07876a8fd5e5536c3b52
Pavel Krayzel <pavel.krayzel@hotjar.com>
2024-09-19 19:15:56 +0200

    Fixing problem with redis on live

--- api/redis.go
+++ api/redis.go
@@ -11,17 +11,21 @@ import (
 var rdb *redis.Client
 var ctx = context.Background()
 
+// InitRedis initializes the Redis connection
 func InitRedis() {
+	redisURL := os.Getenv("REDIS_URL") // Ensure REDIS_URL does not contain redis://
+
 	rdb = redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_URL"),
-		Password: os.Getenv("REDIS_PASSWORD"),
-		DB:       0,
+		Addr:     redisURL,
+		Password: os.Getenv("REDIS_PASSWORD"), // Redis password from the environment
+		DB:       0,                           // Use default DB
 	})
 
+	// Test the connection
 	pong, err := rdb.Ping(ctx).Result()
 	if err != nil {
-		log.Fatalf("Error connecting to Redis: %v", err) // This will terminate the app if Redis is down
+		log.Fatalf("Error connecting to Redis: %v", err)
 	} else {
-		log.Printf("Connected to Redis: %s", pong)
+		log.Printf("Connected to Redis at %s: %s", redisURL, pong)
 	}
 }
